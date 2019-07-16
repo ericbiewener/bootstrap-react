b5c1e01ccfc40c20789789f2443d27c726ae5cce
Eric Biewener <ebiewener@gmail.com>
2019-07-15 22:17:11 -0700

    Initial attempt to got HMR working

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
+import 'react-hot-loader'
 import * as React from 'react'
 import ReactDOM from 'react-dom'
-
-const App = () => <div>App!</div>
+import { App } from './App'
 
 ReactDOM.render(<App />, document.getElementById('_react-root'))
