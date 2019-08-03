1f294d541f92bf7425b19ed5cea5b4c57ea5bab9
Eric Biewener <ebiewener@gmail.com>
2019-08-02 22:41:57 -0700

    Hot reload working

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
-import 'react-hot-loader'
+import { App } from './App'
 import * as React from 'react'
 import ReactDOM from 'react-dom'
-import { App } from './App'
 
 ReactDOM.render(<App />, document.getElementById('_react-root'))
