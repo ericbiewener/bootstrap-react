8f0fc91297435cb08ddb849c6e0608e0b69f37bc
Eric Biewener <eric.biewener0@walmart.com>
2023-01-25 06:00:47 -0800

    remove external eslint config dependency

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,50 @@
 module.exports = {
-  extends: "@ericbiewener/eslint-config-ts",
-  ignorePatterns: ["dist", "node_modules", "*.js", "webpack.config.ts"],
+  extends: ["prettier"],
+  overrides: [
+    {
+      files: ["**/*.{ts,tsx}"],
+      extends: ["plugin:@typescript-eslint/recommended", "prettier"],
+      plugins: ["@typescript-eslint"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: ["./tsconfig.json"],
+      },
+      rules: {
+        "no-undef": 0, // TS reports this itself
+        "interface-over-type-literal": 0,
+        "require-array-sort-compare": 0,
+        "@typescript-eslint/no-explicit-any": 0,
+        "no-unused-vars": 0,
+        "@typescript-eslint/no-unused-vars": 0,
+        "@typescript-eslint/prefer-interface": 0,
+        "@typescript-eslint/no-use-before-define": [
+          "error",
+          { functions: false, classes: false },
+        ],
+        "@typescript-eslint/explicit-function-return-type": 0,
+        "@typescript-eslint/no-non-null-assertion": 0,
+        "@typescript-eslint/ban-ts-ignore": 0,
+        "@typescript-eslint/explicit-member-accessibility": [
+          "error",
+          { accessibility: "no-public" },
+        ],
+        "@typescript-eslint/explicit-module-boundary-types": 0,
+        "no-unused-expressions": 0,
+        "@typescript-eslint/no-unused-expressions": 2,
+        "@typescript-eslint/ban-ts-comment": 0,
+        "@typescript-eslint/camelcase": 0,
+        "@typescript-eslint/no-unnecessary-type-constraint": 0,
+        "@typescript-eslint/no-var-requires": 0,
+      },
+    },
+    {
+      files: ["**/*.test.{ts,tsx}"],
+      env: {
+        jest: true,
+      },
+      rules: {
+        "@typescript-eslint/no-object-literal-type-assertion": 0,
+      },
+    },
+  ],
 };
